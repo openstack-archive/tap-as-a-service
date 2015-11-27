c6d91426a654aa2a2e7fe9fc3b239e305e0def63
Reedip Banerjee <reedip.banerjee@nectechnologies.in>
2015-11-27 08:46:21 +0530

    Remove old TaaS CLI repo
    With the introduction of NeutronExtension for TaaS, the previous
    TaaS CLI repository is not required anymore.
    Therefore this patch removes the old TaaS CLI repo.
    
    Change-Id: Iade483ab93583c5699ceee2259e3c309c3c5bb10
    Depends-On: I8ce10e3001c46e24980dc701540cca8e837c3700

--- neutron_taas/taas_client/tapflow.py
+++ neutron_taas/taas_client/tapflow.py
@@ -76,7 +76,7 @@ class ListTapFlow(extension.ClientExtensionList, TapFlow):
     """List tap flows."""
 
     shell_command = 'tap-flow-list'
-    list_columns = ['id', 'name','source_port', 'tap_service_id']
+    list_columns = ['id', 'name', 'source_port', 'tap_service_id']
     pagination_support = True
     sorting_support = True
 
