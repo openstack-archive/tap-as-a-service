2b56cd2327a944a28ad15c108f9ff5445b03039e
reedip <reedip.banerjee@nectechnologies.in>
2016-03-03 16:12:36 +0900

    Fix i18n translation imports
    As i18n in Neutronclient was migrated to _i18n,
    incorporate the same changes here.
    
    Change-Id: I54ee8f87ae52f60fe20ed0b4292f61a2159d1db5

--- neutron_taas/taas_client/tapflow.py
+++ neutron_taas/taas_client/tapflow.py
@@ -14,9 +14,9 @@
 #    under the License.
 #
 
+from neutronclient._i18n import _
 from neutronclient.common import extension
 from neutronclient.common import utils
-from neutronclient.i18n import _
 from neutronclient.neutron import v2_0 as neutronv20
 
 
--- neutron_taas/taas_client/tapservice.py
+++ neutron_taas/taas_client/tapservice.py
@@ -14,8 +14,8 @@
 #    under the License.
 #
 
+from neutronclient._i18n import _
 from neutronclient.common import extension
-from neutronclient.i18n import _
 from neutronclient.neutron import v2_0 as neutronv20
 
 
