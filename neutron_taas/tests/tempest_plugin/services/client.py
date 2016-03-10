13aec2f40a6e8dd8e177431d3639bb83a6d01b02
reedip <reedip.banerjee@nectechnologies.in>
2016-03-10 13:58:32 +0900

    TrivialFix: Move from tempest to tempest.lib
    As [1] has merged, to prevent future tempest gate failures,
    we can move tempest to tempest.lib for the Network Client.
    
    [1]: https://review.openstack.org/#/c/290108/
    
    Change-Id: I90e7ad4df13a8214d911005feac94f57c8b9a4b9

--- neutron_taas/tests/tempest_plugin/services/client.py
+++ neutron_taas/tests/tempest_plugin/services/client.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from tempest.services.network.json import base
+from tempest.lib.services.network.json import base
 
 
 class TapServicesClient(base.BaseNetworkClient):
