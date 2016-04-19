126e89d3181f4d624820305ced53a09c87d9fb17
Yoichiro Iura <iura.yoichiro@jp.fujitsu.com>
2016-04-19 09:33:45 +0000

    Support service framework to separate plugin logic
    This fix supports service framework in order to separate
    the plugin code into backend specific and generic part.
    
    Change-Id: I66d0f4a9ce5453ec7ca0bf0fe52f24a3a2e3c8ea
    Closes-Bug: #1567770

---
+++ neutron_taas/services/taas/service_drivers/service_driver_context.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2016 Midokura SARL.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+class ServiceDriverContext(object):
+    """ServiceDriverContext context base class"""
+    def __init__(self, service_plugin, plugin_context):
+        self._plugin = service_plugin
+        self._plugin_context = plugin_context
+
+
+class TapServiceContext(ServiceDriverContext):
+
+    def __init__(self, service_plugin, plugin_context, tap_service):
+        super(TapServiceContext, self).__init__(service_plugin, plugin_context)
+        self._tap_service = tap_service
+        self._tap_id_association = self._plugin.get_tap_id_association(
+            plugin_context, tap_service_id=tap_service['id'])
+
+    @property
+    def tap_service(self):
+        return self._tap_service
+
+    @property
+    def tap_id_association(self):
+        return self._tap_id_association
+
+
+class TapFlowContext(ServiceDriverContext):
+
+    def __init__(self, service_plugin, plugin_context, tap_flow):
+        super(TapFlowContext, self).__init__(service_plugin, plugin_context)
+        self._tap_flow = tap_flow
+
+    @property
+    def tap_flow(self):
+        return self._tap_flow
