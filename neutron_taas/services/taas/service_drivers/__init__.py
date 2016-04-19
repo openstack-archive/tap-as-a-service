f3406ee5c4c80ee7e73467fe049d93226c33b4eb
Yoichiro Iura <iura.yoichiro@jp.fujitsu.com>
2016-04-19 09:33:45 +0000

    Support service framework to separate plugin logic
    This fix supports service framework in order to separate
    the plugin code into backend specific and generic part.
    
    Change-Id: I66d0f4a9ce5453ec7ca0bf0fe52f24a3a2e3c8ea
    Closes-Bug: #1567770

---
+++ neutron_taas/services/taas/service_drivers/__init__.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2016 Midokura SARL.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import abc
+
+from oslo_log import log as logging
+import six
+
+LOG = logging.getLogger(__name__)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class TaasBaseDriver(object):
+
+    def __init__(self, service_plugin):
+        self.service_plugin = service_plugin
+
+    @property
+    def service_type(self):
+        pass
+
+    @abc.abstractmethod
+    def create_tap_service_precommit(self, context):
+        pass
+
+    @abc.abstractmethod
+    def delete_tap_service_precommit(self, context):
+        pass
+
+    @abc.abstractmethod
+    def create_tap_flow_precommit(self, context):
+        pass
+
+    @abc.abstractmethod
+    def delete_tap_flow_precommit(self, context):
+        pass
+
+    @abc.abstractmethod
+    def create_tap_service_postcommit(self, context):
+        pass
+
+    @abc.abstractmethod
+    def delete_tap_service_postcommit(self, context):
+        pass
+
+    @abc.abstractmethod
+    def create_tap_flow_postcommit(self, context):
+        pass
+
+    @abc.abstractmethod
+    def delete_tap_flow_postcommit(self, context):
+        pass
