5208e86d4ff00abd7f77f7756abac9303e9bb003
cheng <tangch318@gmail.com>
2016-07-30 04:18:53 -0400

    Fixes enable taas id reuse
    This patch add an new TaasIdAllocationPools
    table to record taas id allocate ranges
    
    Change-Id: I93e64eae7794ee4a315ddda2ea85457e8e9597be
    closes-Bug: #1501953

---
+++ neutron_taas/db/migration/alembic_migration/versions/newton/expand/bac61f603e39_add_taasidallocationpool_to_support_tap_id_reuse.py
@@ -0,0 +1,37 @@
+# Copyright 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""add TaasIdAllocationPool to support tap id reuse
+
+Revision ID: bac61f603e39
+Revises: 04625466c6fa
+Create Date: 2016-07-27 09:31:54.200165
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'bac61f603e39'
+down_revision = '04625466c6fa'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'taas_id_allocation_pools',
+        sa.Column('first_taas_id', sa.INTEGER, primary_key=True),
+        sa.Column('last_taas_id', sa.INTEGER, primary_key=True))
+    op.alter_column('tap_id_associations', 'taas_id', autoincrement=False,
+                    existing_type=sa.INTEGER, nullable=False)
