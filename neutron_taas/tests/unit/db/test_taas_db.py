43c724e06fc9fc132ea70fc6f14d8f6f959ac1b0
Gary Kotton <gkotton@vmware.com>
2017-03-01 04:38:39 -0800

    Use neutron-lib's context module
    Depends-on: I48cf45dc1b07035d952152eac2548a3bd9fc2832
    Change-Id: I5a279a3c86d2048daeb666bff5635afd7cd13f75

--- neutron_taas/tests/unit/db/test_taas_db.py
+++ neutron_taas/tests/unit/db/test_taas_db.py
@@ -13,9 +13,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron import context
 from neutron.tests.unit import testlib_api
 
+from neutron_lib import context
 from oslo_utils import importutils
 from oslo_utils import uuidutils
 
--- neutron_taas/tests/unit/services/taas/test_taas_plugin.py
+++ neutron_taas/tests/unit/services/taas/test_taas_plugin.py
@@ -18,11 +18,11 @@ import contextlib
 import mock
 import testtools
 
+from neutron_lib import context
 from oslo_utils import uuidutils
 
 import neutron.common.rpc as n_rpc
 import neutron.common.utils as n_utils
-from neutron import context
 from neutron.tests.unit import testlib_api
 
 import neutron_taas.db.taas_db  # noqa
