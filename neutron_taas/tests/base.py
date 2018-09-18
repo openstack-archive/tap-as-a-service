89db5d3e032c0b8cbdb647a6ad4dd6cff65d17c2
Deepak Tiwari <deepak.tiwari@aricent.com>
2018-09-19 02:59:34 +0530

    Add SRIOV mirroring support to Tap as a Service.
    The following patch allows VF to VF mirroring in Tap-as-a-Service
    Code-changes are applicable for new tap agent driver for sriov on
    Intel i40e nic.
    
    Vlan Mirror input parameter is part of tap-flow-create API
    
    The current TaaS SRIOV driver is based on Intel i40e NIC driver with
    following requirements:-
    
    hardware: Intel Ethernet Network Adapter XXV710 (25GbE)
    Driver: Intel i40e v4.16.0
    
    Ref Spec: openstack/neutron-specs/specs/rocky/port-mirroring-sriov-vfs.rst
    Commit: https://review.openstack.org/#/c/574477/
    
    Change-Id: Id3aa83d7e1e22ae1806cef0c93e5dd61169c6735

--- neutron_taas/tests/base.py
+++ neutron_taas/tests/base.py
@@ -1,5 +1,4 @@
-# Copyright (C) 2015 Ericsson AB
-# Copyright (c) 2015 Gigamon
+# Copyright (C) 2018 AT&T
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -12,20 +11,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+from oslotest import base
 
 
-# OVS tables used by TaaS in br-tap
-TAAS_RECV_LOC = 1
-TAAS_RECV_REM = 2
-
-# OVS tables used by TaaS in br-tun
-TAAS_SEND_UCAST = 30
-TAAS_SEND_FLOOD = 31
-TAAS_CLASSIFY = 35
-TAAS_DST_CHECK = 36
-TAAS_SRC_CHECK = 37
-TAAS_DST_RESPOND = 38
-TAAS_SRC_RESPOND = 39
-
-# OVS TaaS extension driver type
-EXTENSION_DRIVER_TYPE = 'ovs'
+class TaasTestCase(base.BaseTestCase):
+    """Test case base class for all unit tests."""
