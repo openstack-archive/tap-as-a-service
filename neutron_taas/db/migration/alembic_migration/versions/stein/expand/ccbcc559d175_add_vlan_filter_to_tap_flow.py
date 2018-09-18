9dad40ac2da864a4a3540dc9a2ced9692af8916b
Deepak Tiwari <deepak.tiwari@aricent.com>
2018-09-19 02:59:34 +0530

    Add SRIOV mirroring support to Tap as a Service.
    The following patch allows VF to VF mirroring in Tap-as-a-Service
    Code-changes are applicable for new tap agent driver for sriov on
    Intel i40e nic.
    
    Vlan Mirror input parameter is part of tap-flow-create API
    
    The current TaaS SRIOV driver is based on Intel i40e NIC driver with
    following requirements:-
    
    hardware: Intel Ethernet Network Adapter XXV710 (25GbE)
    Driver: Intel i40e v4.16.0
    
    Ref Spec: openstack/neutron-specs/specs/rocky/port-mirroring-sriov-vfs.rst
    Commit: https://review.openstack.org/#/c/574477/
    
    Change-Id: Id3aa83d7e1e22ae1806cef0c93e5dd61169c6735

--- neutron_taas/db/migration/alembic_migration/versions/stein/expand/ccbcc559d175_add_vlan_filter_to_tap_flow.py
+++ neutron_taas/db/migration/alembic_migration/versions/stein/expand/ccbcc559d175_add_vlan_filter_to_tap_flow.py
@@ -1,6 +1,4 @@
 # Copyright (C) 2018 AT&T
-# Copyright (C) 2015 Midokura SARL.
-# All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -14,7 +12,24 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-TAAS = 'TAAS'
+"""add_vlan_filter_to_tap_flow
 
-# Complete VLAN Id Range
-VLAN_RANGE = '0-4095'
+Revision ID: ccbcc559d175
+Revises: fddbdec8711a
+Create Date: 2018-09-18 19:33:32.119458
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ccbcc559d175'
+down_revision = 'fddbdec8711a'
+
+from alembic import op
+import sqlalchemy as sa
+
+TABLE_NAME = 'tap_flows'
+
+
+def upgrade():
+    op.add_column(TABLE_NAME, sa.Column('vlan_filter', sa.String(1024),
+                                        nullable=True))
